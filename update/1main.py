fa8989458aa35717983b56bd67846b503d27771a
MRyutaro <m2002r1028@icloud.com>
2022-08-03 21:49:19 +0900

    clean the program

--- update/1main.py
+++ update/1main.py
@@ -2,45 +2,37 @@ import cv2
 import matplotlib.pyplot as plt
 
 # 入力ファイルのパスを指定
-file = "./data/a.png"
-
-# 認識範囲
-xmin, xmax = 540, 890
-ymin, ymax = 40, 130
+file = "./data/sample.png"
 
 # 読み込み
 image = cv2.imread(file)
+image = cv2.resize(image, (768, 432))
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-# 画像のサイズ縮小
-# height = image.shape[0]
-# width = image.shape[1]
-# image = cv2.resize(image,(round(width/4), round(height/4)))
+x_min, x_max = 216, 356
+y_min, y_max = 16, 52
 
-image_copy1 = image[ymin:ymax, xmin:xmax]
-# グレースケール化
-image_copy1 = cv2.cvtColor(image_copy1, cv2.COLOR_BGR2GRAY)
 
-# 閾値処理
-ret, thresh = cv2.threshold(image_copy1, 95, 255, cv2.THRESH_BINARY)
+if __name__ == "__main__":
+    image_copy1 = image[y_min:y_max, x_min:x_max]
+    # グレースケール化
+    image_copy1 = cv2.cvtColor(image_copy1, cv2.COLOR_BGR2GRAY)
+    # 閾値処理
+    ret, thresh = cv2.threshold(image_copy1, 95, 255, cv2.THRESH_BINARY)
 
-# 輪郭検出 （cv2.ChAIN_APPROX_SIMPLE）
-contours1, hierarchy1 = cv2.findContours(
-    thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # 輪郭検出
+    contours1, hierarchy1 = cv2.findContours(
+        thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-# 輪郭の描画
-# contours1[0] += xmin
-# contours1[1] += ymin
-print(len(contours1))
-for i in range(len(contours1)):
-    print(contours1[i][0][0])
-    print(contours1[i][0][1])
+    # 輪郭位置の補正
+    for i in range(len(contours1)):
+        for j in range(len(contours1[i])):
+            contours1[i][j][0][0] += x_min
+            contours1[i][j][0][1] += y_min
 
-cv2.drawContours(image, contours1, -1, (0, 255, 0), 2, cv2.LINE_AA)
+    # 結果の描画
+    cv2.drawContours(image, contours1, -1, (0, 255, 0), 2, cv2.LINE_AA)
+    cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (255, 0, 0), 2)
 
-# 認識範囲に赤枠
-cv2.rectangle(image, (xmin, ymin), (xmax, ymax), (255, 0, 0), 2)
-
-# 入力画像の表示
-plt.imshow(image)
-plt.show()
+    plt.imshow(image)
+    plt.show()
