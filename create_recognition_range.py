2bc7dc179592904048dc7ec1c3d90fe546c6e1f3
MRyutaro <m2002r1028@icloud.com>
2022-09-03 17:19:27 +0900

    フローの大元を書いた

--- create_recognition_range.py
+++ create_recognition_range.py
@@ -1,3 +1,13 @@
 # video
 # ->
-# range
+# game_transition, label, {"target": "range_lsit"}
+
+# まずはrecrangleで場所を指定する。
+# そのあと切り取りをしてチェックをする。
+# 色がでた数を表示できるようにする。
+# unique, counts = np.unique(clipped_range, return_counts=True)
+# result = np.column_stack((unique, counts))
+# ↑これ
+# game_trantision, label, {"target": "range_lsit"}
+# label判定するのに何か所か判定場所が必要。
+# それがtarget
