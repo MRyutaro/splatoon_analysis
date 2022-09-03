e55d7d54f7d82df2e0d696f6df2d3054d1cfdb63
MRyutaro <m2002r1028@icloud.com>
2022-09-04 01:53:30 +0900

    ピンチのときの範囲を記録

--- create_recognition_range.py
+++ create_recognition_range.py
@@ -11,3 +11,41 @@
 # game_trantision, label, {"target": "range_lsit"}
 # label判定するのに何か所か判定場所が必要。
 # それがtarget
+
+import cv2
+
+
+if __name__ == "__main__":
+    # ピンチの判定
+    # image_path = "./data/image/friend_pinch.png"
+    # image_path = "./data/image/enemy_pinch.png"
+
+    # ゲーム開始
+    image_path = "./data/image/start.png"
+    image = cv2.imread(image_path)
+    image = cv2.resize(image, (768, 432))
+
+    # ピンチの判定--------------------------------------------------------
+    # # friend pinch
+    # cv2.rectangle(image, (175, 16), (220, 33), (255, 0, 0), 1)
+    # # ika icons in friend pinch
+    # cv2.rectangle(image, (227, 13), (348, 42), (255, 0, 0), 1)
+    # cv2.rectangle(image, (414, 10), (569, 45), (255, 0, 0), 1)
+
+    # # enemy pinch
+    # cv2.rectangle(image, (545, 16), (590, 33), (255, 0, 0), 1)
+    # # ika icons in enemy pinch
+    # cv2.rectangle(image, (197, 10), (352, 45), (255, 0, 0), 1)
+    # cv2.rectangle(image, (418, 13), (539, 42), (255, 0, 0), 1)
+
+    # ゲームの開始の判定--------------------------------------------------
+    cv2.rectangle(image, (227, 13), (348, 42), (255, 0, 0), 1)
+
+    # ゲームの終了の判定--------------------------------------------------
+
+    # マップを開いているかの判定------------------------------------------
+
+    # image = cv2.resize(image, (1920, 1080))
+    cv2.imshow("image", image)
+    if cv2.waitKey(0) == 27:
+        exit()
