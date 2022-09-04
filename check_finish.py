f8136764a9f5bb70b5617c667c82410ed386e0be
MRyutaro <m2002r1028@icloud.com>
2022-09-04 17:00:44 +0900

    クラス継承

---
+++ check_finish.py
@@ -0,0 +1,23 @@
+import cv2
+import time
+
+from image_recognition_v1 import ImageRecognition
+
+if __name__ == "__main__":
+    # movie = cv2.VideoCapture(1)
+    movie = cv2.VideoCapture('./data/video/area_trim1.mp4')
+    game_finishes = ImageRecognition("game", "finishes")
+
+    while movie.isOpened:
+        ret, frame = movie.read()
+        frame = cv2.resize(frame, (768, 432))
+
+        if not game_finishes.is_equal(frame):
+            print("game starts")
+
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) == 27:
+            break
+        time.sleep(0.02)
+
+    movie.release()
