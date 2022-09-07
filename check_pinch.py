e699d166a7276c3299225335c61f9b0577c9cad9
MRyutaro <m2002r1028@icloud.com>
2022-09-08 02:38:05 +0900

    関数名の変更

--- check_pinch.py
+++ check_pinch.py
@@ -7,11 +7,11 @@ class CheckPinch(ImageRecognition):
     def is_equal(self, image):
         # print("True or False")
         # print(len(self.range)) -> 1
-        binary_image = self.binary(image)
-        clipped_binary_image = self.clip_image(
+        binary_image = self._binary(image)
+        clipped_binary_image = self._clip_image(
             binary_image, self.range[0]["target"][0], self.range[0]["target"][1])
-        if self.image_is_equal(clipped_binary_image, self.range[0]["color"][0]):
-            self.show_rectangle(image)
+        if self._image_is_equal(clipped_binary_image, self.range[0]["color"][0]):
+            self._draw_rectangle(image)
             return True
         else:
             return False
@@ -20,15 +20,15 @@ class CheckPinch(ImageRecognition):
 if __name__ == "__main__":
     # movie = cv2.VideoCapture(1)
     movie = cv2.VideoCapture('./data/video/area_trim1.mp4')
-    friend_is_in_pinch = CheckPinch("in_pinch", "is friend", 0.8)
-    # enemy_is_in_pinch = CheckPinch("in_pinch", "is enemy", 0.9)
+    friend_is_in_pinch = CheckPinch("in_pinch", "is friend")
+    enemy_is_in_pinch = CheckPinch("in_pinch", "is enemy")
 
     while movie.isOpened:
         ret, frame = movie.read()
         frame = cv2.resize(frame, (768, 432))
 
         print(friend_is_in_pinch.is_equal(frame))
-        # print(enemy_is_in_pinch.is_equal(frame))
+        print(enemy_is_in_pinch.is_equal(frame))
 
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) == 27:
